810e2beeb1cf2049a8aef573c1f6435a17d7200b
egprogram <runrunrinmaru@gmail.com>
2023-01-08 03:08:39 +0000

    serverless適応2

--- index.js
+++ index.js
@@ -65,17 +65,13 @@ const s3Upload = async (decodeData, fileName) => {
   }) 
 }
 
-app.get("/healthcheck", (req, res) => {
-  res.send({ status: true })
-})
-
 app.post("/webhook", async (req, res) => {
   if (req.body.events[0].type === "message" && req.body.events[0].message.text) {
     const words = req.body.events[0].message.text.split('\n')
     if(words[0] === 'qrcode'){
       // QRコードを作成
-      const str = req.body.events[0].message.text
-      const width = 200
+      const str = words[1]
+      const width = words[2] || 100 // デフォルトを100x100pxにする
       const qrcodeBuffer = await generateQrcode(str, width)
 
       // S3にアップロード
@@ -130,10 +126,6 @@ app.post("/webhook", async (req, res) => {
       request.end()
     }
   }
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
 })
 
 module.exports.handler = serverless(app)
